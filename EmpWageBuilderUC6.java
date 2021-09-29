aa33b6183210a686bf21db9ddb61eb9ca96fff62
Amit Sisodiya <amitsisodiya275@gmail.com>
2021-09-29 22:18:13 +0530

    Added: a new file of UC6 in that calculating wages till some condition met.

---
+++ EmpWageBuilderUC6.java
@@ -0,0 +1,45 @@
+package com.bridgelab.empwagebuilder;
+
+public class EmpWageBuilderUC6 {
+	// Constants
+	static final int IS_FULL_TIME = 1;
+	static final int IS_PART_TIME = 2;
+	static final int PER_HR_WAGE = 20;
+	static final int MAX_WORKING_DAYS = 20;
+	static final int MAX_WORKING_HRS = 100;
+
+	public static void main(String[] args) {
+		calculateWage();
+	}
+
+	public static void calculateWage() {
+		// Variables
+		int empHrs = 0;
+		int empWage = 0;
+		int totalEmpWage = 0;
+		int totalEmpHr = 0;
+		int totalEmpWorkingDays = 0;
+
+		while (totalEmpWorkingDays < MAX_WORKING_DAYS && totalEmpHr <= MAX_WORKING_HRS) {
+			int empCheck = (int) Math.floor(Math.random() * 10 % 3);
+			totalEmpWorkingDays++;
+			switch (empCheck) {
+			case IS_FULL_TIME:
+				empHrs = 8;
+				break;
+			case IS_PART_TIME:
+				empHrs = 4;
+				break;
+			default:
+				empHrs = 0;
+			}
+			totalEmpHr += empHrs;
+			empWage = empHrs * PER_HR_WAGE;
+
+			System.out.println(
+					"Day " + totalEmpWorkingDays + " Working Hours " + empHrs + " , & Todays wage is " + empWage);
+		}
+		totalEmpWage = totalEmpHr * PER_HR_WAGE;
+		System.out.println("Total wage = " + totalEmpWage);
+	}
+}
